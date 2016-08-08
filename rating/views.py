0b3b4880b316669f0bb47a1e9715f9b5de67ebd0
Anett Gesierich <anett@emsuiko.de>
2016-08-08 22:31:03 +0200

    use django shortcut render

--- rating/views.py
+++ rating/views.py
@@ -1,45 +1,28 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 from .models import *
 
 
 def index(request):
     cinema_list = Cinema.objects.all()
-    template = loader.get_template('rating/index.html')
-    context = {
-        'cinema_list': cinema_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'rating/index.html', {'cinema_list': cinema_list, })
 
 
 def cinema(request, cinema_id):
     cinema = Cinema.objects.get(id=cinema_id)
     hall_list = Hall.objects.filter(cinema=cinema)
 
-    template = loader.get_template('rating/cinema.html')
-    context = {
-        'hall_list': hall_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'rating/cinema.html', {'hall_list': hall_list, })
 
 
 def hall(request, cinema_id, hall_id):
     row_list = Row.objects.filter(hall__id=hall_id)
-
-    template = loader.get_template('rating/hall.html')
-    context = {
-        'row_list': row_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'rating/hall.html', {'row_list': row_list, })
 
 
 def seat(request, cinema_id, hall_id, seat_id):
-    seat = Seat.objects.get(id=seat_id)
-
-    template = loader.get_template('rating/seat.html')
-    context = {
-        'seat': seat,
-    }
-    return HttpResponse(template.render(context, request))
+    seat_details = Seat.objects.get(id=seat_id)
+    return render(request, 'rating/seat.html', {'seat': seat_details, })
 
